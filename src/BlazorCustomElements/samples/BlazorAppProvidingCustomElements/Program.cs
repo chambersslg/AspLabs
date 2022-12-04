c356a3113c2faeb88e5300c7acd4bf3eb10ec264
chambersslg <stephen.chambers@eurofinset.com>
2022-12-04 12:52:07 -0800

    Update version

--- src/BlazorCustomElements/samples/BlazorAppProvidingCustomElements/Program.cs
+++ src/BlazorCustomElements/samples/BlazorAppProvidingCustomElements/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using BlazorAppProvidingCustomElements;
+using System.Net.Http;
+using System;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
